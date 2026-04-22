fb00f442adf11f23ece2ddbd05399dd11833bb51
Luc <luc@lucemans.nl>
2026-04-22 12:20:51 +0200

    Update network popover

--- ui/src/components/navbar/networks.tsx
+++ ui/src/components/navbar/networks.tsx
@@ -1,4 +1,5 @@
 import { Popover } from "@kobalte/core/popover";
+import { Link } from "@tanstack/solid-router";
 import { FaSolidNetworkWired } from "solid-icons/fa";
 import { For } from "solid-js";
 
@@ -12,20 +13,23 @@ export const NetworkWidget = () => {
             <Popover.Trigger class="h-full aspect-square flex justify-center items-center hover:bg-surface-alt cursor-pointer">
                 <FaSolidNetworkWired />
             </Popover.Trigger>
-            <Popover.Content>
-                <div class="bg-surface p-3 rounded-md border border-border outline-none w-full max-w-md">
-                    <ul class="divide-y divide-border">
-                        <For each={networksQuery.data?.networks}>
-                            {network => (
-                                <li>
-                                    <button class="py-1">
-                                        {network.network_name}
-                                    </button>
-                                </li>
-                            )}
-                        </For>
-                    </ul>
-                </div>
+            <Popover.Content class="bg-surface p-3 rounded-md border border-border outline-none w-full max-w-md">
+                <ul class="divide-y divide-border">
+                    <For each={networksQuery.data?.networks}>
+                        {network => (
+                            <li>
+                                <button class="py-1">
+                                    {network.network_name}
+                                </button>
+                            </li>
+                        )}
+                    </For>
+                </ul>
+                <Popover.CloseButton>
+                    <Link to="/settings/networks">
+                        Configure
+                    </Link>
+                </Popover.CloseButton>
             </Popover.Content>
         </Popover>
     );
