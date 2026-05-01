3e8a2676990a0ddd881fd7bc8e3a04e86b2e3675
Luc <luc@lucemans.nl>
2026-05-01 22:04:29 +0200

    Improve account token add filtering

--- ui/src/components/account/asset/link.tsx
+++ ui/src/components/account/asset/link.tsx
@@ -1,18 +1,25 @@
 import { FiPlus, FiRefreshCcw, FiX } from "solid-icons/fi";
 import { Component, createMemo, For } from "solid-js";
 
-import { useAccountAssets, useAddAccountAsset, useRemoveAccountAsset } from "#/api/account";
+import { useAccount, useAccountAssets, useAddAccountAsset, useRemoveAccountAsset } from "#/api/account";
 import { useAssets } from "#/api/asset";
 import { AssetPreview } from "#/components/asset/preview";
 import { Modal } from "#/components/dialog";
 
 export const AccountAssetLink: Component<{ account_identity: number; }> = ({ account_identity }) => {
     const assetsQuery = useAssets();
+    const accountQuery = useAccount(() => ({ path: { account_identity } }));
     const accountAssetsQuery = useAccountAssets(() => ({ path: { account_identity } }));
     const linkAsset = useAddAccountAsset<{ account_identity: number; asset_identity: string; }>(({ account_identity, asset_identity }) => ({ path: { account_identity, asset_identity } }));
 
+    const account = createMemo(() => accountQuery.data);
+
     // Filters out assets already linked, and fiat assets (as linking a fiat asset to an evm wallet should not be done)
-    const filteredAssets = createMemo(() => assetsQuery.data?.assets?.filter(asset => !asset.asset_identity.startsWith("fiat:") && !accountAssetsQuery.data?.includes(asset.asset_identity)));
+    const filteredAssets = createMemo(() => assetsQuery.data?.assets?.filter(asset => !asset.asset_identity.startsWith("fiat:") && !accountAssetsQuery.data?.includes(asset.asset_identity)).filter((asset) => {
+        const network_identity = asset.asset_identity.split(":")[1];
+
+        return account()?.networks.includes(Number(network_identity));
+    }));
 
     return (
         <Modal>
