4752e7cacbc6b3808e4e68794543393c78f3ada6
Luc <luc@lucemans.nl>
2026-05-01 22:05:30 +0200

    update sql

--- app/migrations/0005_accounts.sql
+++ app/migrations/0005_accounts.sql
@@ -4,9 +4,3 @@ CREATE TABLE accounts (
     networks TEXT NOT NULL,
     metadata TEXT NOT NULL
 );
-
-CREATE TABLE account_assets (
-    account_identity INTEGER NOT NULL,
-    asset_id TEXT NOT NULL,
-    PRIMARY KEY (account_identity, asset_id)
-);
