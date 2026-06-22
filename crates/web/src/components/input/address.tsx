d5b6fc0e9076b30ce482ea26ccdba0d60ea2f59b
Luc <luc@lucemans.nl>
2026-06-22 10:47:04 +0200

    z

--- crates/web/src/components/input/address.tsx
+++ crates/web/src/components/input/address.tsx
@@ -1,8 +1,20 @@
 import { Accessor, Component, JSX } from "solid-js";
 
+export const ERC3770_IDENTIFIERS = {
+    eth: 1,
+    sep: 11_155_111,
+    base: 8453,
+    op: 10,
+    arb: 42_161,
+    bsc: 56,
+};
+
 export type AddressInputProperties = {
     value?: Accessor<string>;
     onChange?: (value: string) => void;
+    // when set, if the input starts with a safe{wallet} identifier, like `eth:0x123...456` the network identifier will be passed to the function
+    // ERC-3770
+    onNetworkDetected?: (network_identifier: number) => void;
 } & Omit<JSX.InputHTMLAttributes<HTMLInputElement>, "value" | "onChange">;
 
 export const AddressInput: Component<AddressInputProperties> = props => (
