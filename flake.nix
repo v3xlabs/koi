c34794433d2d8641769785711a77edd9a5222fa7
Luc <luc@lucemans.nl>
2026-06-30 23:26:17 +0200

    Improve flake

--- flake.nix
+++ flake.nix
@@ -4,28 +4,45 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    rust-overlay,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
+        overlays = [rust-overlay.overlays.default];
       };
+
+      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [
+          "rust-src"
+          "llvm-tools"
+        ];
+        targets = ["wasm32-unknown-unknown"];
+      };
+
+      rustfmtNightly = pkgs.rust-bin.nightly.latest.rustfmt;
     in {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
-          just
-          cargo
-          rustc
-          rustfmt
+          rustfmtNightly
+          rustToolchain
+          rust-analyzer
           bacon
-          clippy
+          just
+
           nodejs_24
           pnpm_11
+
           pkg-config
           gtk3
           webkitgtk_4_1
